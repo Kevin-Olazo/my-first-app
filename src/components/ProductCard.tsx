7be8d8ab90a8cfdc05b397db93545f1ee8fd5405
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-16 15:41:41 -0500

    [update]: ProductCard details

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { Product } from "@/generated/prisma";
 import Link from "next/link";
+import PriceTag from "./PriceTag";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
@@ -11,10 +13,19 @@ const ProductCard = ({ product }: ProductCardProps) => {
       href={"/products/" + product.id}
       className="card w-full bg-base-100 hover:shadow-xl transition-shadow"
     >
+      <figure>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={800}
+          height={400}
+          className="h-48 object-cover"
+        />
+      </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
         <p>{product.description}</p>
-        <p>S/.{product.price}</p>
+        <PriceTag price={product.price} />
       </div>
     </Link>
   );
