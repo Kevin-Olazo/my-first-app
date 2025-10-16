05344980a1d8a7c789c3d36a66f7e4b42e067fde
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-16 02:12:07 -0500

    [feat]: add-product page, daisyUI and added garden theme

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,12 @@
+const AddProductPage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold mb-3">Add product</h1>
+      <form action="">
+        <input type="text" placeholder="Type here" className="input" />
+      </form>
+    </div>
+  );
+};
+
+export default AddProductPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const rubik = Rubik({
+  subsets: ["latin"], // Specify the desired subsets
+  weight: ["400", "700"], // Specify desired weights, or omit for variable font
+  display: "swap", // Recommended for better performance
 });
 
 export const metadata: Metadata = {
@@ -23,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" data-theme="garden">
+      <body className={`${rubik.className} antialiased`}>{children}</body>
     </html>
   );
 }
