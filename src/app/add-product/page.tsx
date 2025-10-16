a849f08c91dd99243987362b825a47c59215c516
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-16 14:25:43 -0500

    [feat]: createProduct Server Action

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,15 +1,41 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
 export const metadata = {
   title: "Add product",
 };
+
+async function createProduct(formData: FormData) {
+  "use server";
+
+  // Get product inputs
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+  const stock = Number(formData.get("stock") || 0);
+
+  // Validate input are not empty
+  if (!name || !description || !imageUrl || !price || !stock) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price, stock },
+  });
+
+  redirect("/");
+}
 
 const AddProductPage = () => {
   return (
     <div>
       <h1 className="text-lg font-bold mb-3">Add product</h1>
-      <form action="">
+      <form action={createProduct}>
         {/* Product name */}
         <input
           type="text"
+          name="name"
           placeholder="Product name"
           className="input mb-3 w-full"
           required
@@ -35,6 +61,14 @@ const AddProductPage = () => {
           name="price"
           type="number"
           placeholder="Price"
+          className="input mb-3 w-full"
+          step="0.01"
+        />
+        <input
+          required
+          name="stock"
+          type="number"
+          placeholder="Stock"
           className="input mb-3 w-full"
           step="0.01"
         />
