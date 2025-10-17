8cb82e5ad309aeedbc8b306bde84a1c31cfacf06
Kevin-Olazo <kevind.olazo@gmail.com>
2025-10-17 15:02:52 -0500

    [feat] created shoppingCartButton

---
+++ src/app/Navbar/ShoppingCartButton.tsx
@@ -0,0 +1,9 @@
+import { ShoppingCart } from "@/lib/db/cart";
+
+interface ShoppingCartButtonProps {
+  cart: ShoppingCart | null;
+}
+
+export default function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
+  return <div className="dropdown dropdown-end"></div>;
+}
